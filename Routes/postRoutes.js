ab42af16a4170d5b4586fc1a123ac0b19eaad150
asmaakbouchi <asmaakbouchi@gmail.com>
2024-02-19 15:03:57 +0100

    change

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -2,19 +2,13 @@ const express=require("express");
 const router=express.Router();
 const controller=require("../Controllers/postControllers");
 
-app.use((req,res,next)=>{
+router.use((req,res,next)=>{
     const date=new Date();
     let msg=`[${date.toISOString()}] ${req.method} ${req.url}`;
     console.log(msg);
     next();
 })
 
-//Middleware pour error invalide format json
-app.use((err,req,res,next)=>{
-    if(err.status===400){
-      return res.status(400).send("le syntax de json invalide")
-    }
-})
 
 router.get("/",controller.getAllPosts)
 router.post("/",controller.createPost)
@@ -22,10 +16,11 @@ router.get("/:id",controller.getPostById)
 router.put("/:id",controller.updatePost)
 router.delete("/:id",controller.deletePostById)
 
-app.use((err, req, res, next) => {
-    if (err.status == 500) {
-         res.status(500).send('erreur dans le serveur');
-     }
+
+
+
+router.use((err, req, res, next) => {
+    res.status(500).send(err.message);
  });
- 
+
 module.exports=router
--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const routerpost=require("./Routes/postRoutes");
 app.use("/posts",routerpost);
 
 
+// middleware gestion des erreur 
+app.use((req, res) => {
+    res.status(404).send(`La page n'existe pas`);
+});
+
+app.use((err, req, res, next) => {
+    if (err.status === 400) {
+        res.status(400).send("Le Syntaxe de fichier json incorrecte");
+    } else {
+        next(err);
+    }
+});
+
 
 app.listen(port,()=>
 console.log(`Le serveur est executé dans le port ${port}`)

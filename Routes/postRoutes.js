d29c292e1b0ecf522fceb10facc1d2fe0847ce63
asmaakbouchi <asmaakbouchi@gmail.com>
2024-02-19 09:56:21 +0100

    change

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -2,6 +2,19 @@ const express=require("express");
 const router=express.Router();
 const controller=require("../Controllers/postControllers");
 
+app.use((req,res,next)=>{
+    const date=new Date();
+    let msg=`[${date.toISOString()}] ${req.method} ${req.url}`;
+    console.log(msg);
+    next();
+})
+
+//Middleware pour error invalide format json
+app.use((err,req,res,next)=>{
+    if(err.status===400){
+      return res.status(400).send("le syntax de json invalide")
+    }
+})
 
 router.get("/",controller.getAllPosts)
 router.post("/",controller.createPost)
@@ -9,6 +22,10 @@ router.get("/:id",controller.getPostById)
 router.put("/:id",controller.updatePost)
 router.delete("/:id",controller.deletePostById)
 
-
-
+app.use((err, req, res, next) => {
+    if (err.status == 500) {
+         res.status(500).send('erreur dans le serveur');
+     }
+ });
+ 
 module.exports=router
